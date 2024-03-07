cf08e95c141b7084e50152f73a857d844f0f2a53
Ruslan <ruslansharipov19980111@gmail.com>
2024-03-08 02:56:45 +0500

    Накидал пока самую базу. Чтобы попап появлялся, скрывался

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
 function openForm() {
   alert("Hello");
 }
+const startButton = document.getElementById("start-button");
+const popup = document.getElementById("popup");
+const sendData = document.getElementById("sendData");
+
+startButton.addEventListener("click", () => {
+  popup.classList.remove("hidden");
+});
+
+sendData.addEventListener("click", () => {
+  popup.classList.add("hidden");
+});
+
+document.addEventListener("click", (event) => {
+  if (
+    !popup.contains(event.target) &&
+    !startButton.contains(event.target) &&
+    !popup.classList.contains("hidden")
+  ) {
+    popup.classList.add("hidden");
+  }
+});
+
+// alert("щас должно скрыться, классы попапа");
+// alert(popup.classList);
+// console.log(popup.classList);
